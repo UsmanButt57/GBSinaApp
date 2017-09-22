23fb4dc3627570397fe4e18883233728b22bdace
Muhammad Usman Ghani <b612usman@gmail.com>
2017-09-22 12:45:30 +0500

    link empty project on git Hub

---
+++ GPSinaApp/GPSinaAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GPSinaApp
+{
+    public partial class GPSinaAppPage : ContentPage
+    {
+        public GPSinaAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
